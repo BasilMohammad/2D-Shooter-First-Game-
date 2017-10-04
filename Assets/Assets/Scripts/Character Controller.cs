0e3b6ad8caa4211b2f049972c00c54dc91e0f76b
BasilMohammad <Basil.Mohammad@ocsbstudent.ca>
2017-10-04 12:04:03 -0400

    Character movement

---
+++ Assets/Assets/Scripts/Character Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public float maxspeed = 10;
+    bool facingright = true;
+	// Update is called once per frame
+	void fixedUpdate () {
+        float move = Input.GetAxis("Horizontal");
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(move * maxspeed, GetComponent<Rigidbody2D>().velocity.y);
+
+
+	}
+}
