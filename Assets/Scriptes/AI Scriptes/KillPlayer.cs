320787e7e7ede8be64e1600417c3d0b85b88b2d3
BasilMohammad <Basil.Mohammad@ocsbstudent.ca>
2017-10-19 09:58:55 -0400

    Making an AI

--- Assets/Scriptes/AI Scriptes/KillPlayer.cs
+++ Assets/Scriptes/AI Scriptes/KillPlayer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class KillPlayer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
