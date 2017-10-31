874fd658bc1cf57108ecf1d09a24d7aee2fda960
BasilMohammad <Basil.Mohammad@ocsbstudent.ca>
2017-10-31 09:47:02 -0400

    Finished GAME!!!!!

--- Assets/Scriptes/Win.cs
+++ Assets/Scriptes/Win.cs
@@ -1,23 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Win : MonoBehaviour {
     public CharacterCntrl Player;
-    // Use this for initialization
-    // Use this for initialization
+    public GameObject WinParticle;
+    public Text WinText;
     void Start()
     {
         Player = FindObjectOfType<CharacterCntrl>();
     }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name == "Player")
         {
+            WinText.text = "You Win!";
             Debug.Log("YOU WIN !!" + transform.position);
             Player.enabled = false;
-
+            Player.gameObject.SetActive(false);
+            Instantiate(WinParticle, Player.transform.position, Player.transform.rotation);
         }
     }
 }
